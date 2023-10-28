81a986fd933751bd435ae4cb19683d95b17fe32a
Ranj Erusa <Ranjith.erusa@gmail.com>
2023-10-28 16:55:24 -0400

    change file

--- azurespeechservice/texttospeech.php
+++ azurespeechservice/texttospeech.php
@@ -16,6 +16,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'){
 <!DOCTYPE html>
 <html>
 <head>
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+<meta http-equiv="Pragma" content="no-cache" />
+<meta http-equiv="Expires" content="0" />
     <title>Text to Speech</title>
 </head>
 <body>
