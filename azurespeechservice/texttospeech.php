6caebbac908b5675839df83880d6cfd19d98b62c
Ranj Erusa <Ranjith.erusa@gmail.com>
2023-10-30 23:02:48 -0400

    refactor code

--- azurespeechservice/texttospeech.php
+++ azurespeechservice/texttospeech.php
@@ -2,11 +2,12 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST'){
     exec("rm output.mp3");
     $text = $_POST['input'];
+    $timestamp = $_POST['gmtstamp'];
     $url = "https://eastus.tts.speech.microsoft.com/cognitiveservices/v1";
     $subscription_key = "e8413e6a1ad84a46b6eeb4bb3d01a843";
     $output_format = "audio-16khz-128kbitrate-mono-mp3";
     $data = "<speak version='1.0' xml:lang='en-US'><voice xml:lang='en-US' xml:gender='Female' name='en-US-JennyNeural'>$text</voice></speak>";
-    $command = "curl --location --request POST \"$url\" --header \"Ocp-Apim-Subscription-Key: $subscription_key\" --header \"Content-Type: application/ssml+xml\" --header \"X-Microsoft-OutputFormat: $output_format\" --header \"User-Agent: curl\" --data-raw \"$data\" --output output.mp3";
+    $command = "curl --location --request POST \"$url\" --header \"Ocp-Apim-Subscription-Key: $subscription_key\" --header \"Content-Type: application/ssml+xml\" --header \"X-Microsoft-OutputFormat: $output_format\" --header \"User-Agent: curl\" --data-raw \"$data\" --output \"$timestamp\"-output.mp3";
     $response = exec($command);
     
     
@@ -23,12 +24,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'){
 </head>
 <body>
     <form method="post">
+        <input type="hidden" name="gmtstamp" value="<?php gmdate("Y-m-d\TH:i:s\Z"); ?>" />
         <label for="input">Enter text:</label>
         <input type="text" name="input" id="input">
         <button type="submit">Submit</button> 
     </form>
     <?php
-         echo "<audio controls ><source src='output.mp3' type='audio/mpeg'></audio>";
+         echo "<audio controls ><source src='<?php $timestamp?>-output.mp3' type='audio/mpeg'></audio>";
     ?>
 
 </body>
